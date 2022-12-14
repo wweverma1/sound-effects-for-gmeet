1a0d9bba686a042e3934e5cdc74c8f5e7b6601f8
wweverma1 <wweverma1@gmail.com>
2022-12-14 18:19:14 +0530

    javascript for processing the gmeet

---
+++ script.js
@@ -0,0 +1,20 @@
+function fetchMeet() 
+{
+    const btn = document.getElementById('action-button');
+    btn.innerHTML = '<i class="fa fa-circle-o-notch fa-spin"></i> Fetching';
+    btn.disabled = true;
+    const error = document.getElementById('error-message');
+    const URL = window.location.toString().split("/");
+    const domain = URL.at(-2);
+    if (domain != "meet.google.com")
+    {
+        error.innerHTML = "Switch to a Google Meet and retry";
+        btn.innerHTML = "Detect Meet"
+        btn.disabled = false;
+        return;
+    }
+    const meetCode = URL.at(-1);
+    const success = document.getElementById('success-message');
+    success.innerHTML = meetCode;
+    btn.innerHTML = "Meet Detected"
+}
